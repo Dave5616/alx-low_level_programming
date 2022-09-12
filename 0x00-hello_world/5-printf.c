d25d958ed47830e775c93064d5fed1269ca2168c
Dave5616 <dawitalebelakele@gmail.com>
2022-09-12 14:15:25 -0700

    nice

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+1   /*
+2   * File: 5-printf.c
+3   * Auth: Abdulhakeem A Yisa
+4   */
+5   
+6   #include <stdio.h>
+7
+8   /**
+9   * main - Prints "with proper grammar, but the outcome is a piece of art"
+10  *
+11  * Return: Always 0.
+12  */
+13  int main(void)
+14  {
+15	printf("with proper grammar, but the outcome is a piece of art,\n");
+16	return (0);
+17   }
