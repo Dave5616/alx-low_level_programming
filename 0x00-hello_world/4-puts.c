bfa275d6520dd69a3d3b322e95201d44559b54a8
Dave5616 <dawitalebelakele@gmail.com>
2022-09-12 13:57:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+1 /*
+2  *  * File: 4-put.c
+3  *   * Auth: Abdulhakeem A Yisa
+4  *    */
+5
+6 #include <stdio.h>
+7
+8 /**
+9  *  * main - Prints "Programming is like building a multilingual puzzle"
+10 *
+11 * Return: Always 0.
+12 */
+13 int main(void)
+14 {
+15         puts("\"Programming is like building a multilingual puzzle");
+16	   return (0);
+
+17 }
